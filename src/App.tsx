739bc20c0cb5c21966791f34fa0ffb9b3b8c5286
Ashley <ashley1314.li@gmail.com>
2021-12-09 11:16:27 +1100

    replace the data

--- src/App.tsx
+++ src/App.tsx
@@ -17,18 +17,84 @@ export type CartItemType = {
   amount: number;
 };
 
-const getProducts = async (): Promise<CartItemType[]> => {
-  return await (await fetch("https://fakestoreapi.com/products")).json();
-};
+// const getProducts = async (): Promise<CartItemType[]> => {
+//   return await (await fetch("https://fakestoreapi.com/products")).json();
+// };
+
+const inventory: CartItemType[] = [
+  {
+    id: 1,
+    title: "Sonar Sunglass",
+    description: "something",
+    image: "imgs/shades1.jpg",    
+    price: 600.00,
+  } as CartItemType,
+  {
+    id: 2,
+    title: "Shield Sunglass",
+    description: "something",
+    image: "imgs/shades2.jpg",
+    price: 450.00,
+  } as CartItemType,
+  {
+    id: 3,
+    title: "Warp Sunglass",
+    description: "something",
+    image: "imgs/shades3.jpg",
+    price: 999.00,
+  } as CartItemType,
+  {
+    id: 4,
+    title: "Pilot Sunglass",
+    description: "something",
+    image: "imgs/shades4.jpg",
+    price: 1250.00,
+  } as CartItemType,
+  {
+    id: 5,
+    title: "Volcano Sunglass",
+    description: "something",
+    image: "imgs/shades5.jpg",
+    price: 499.00,
+  } as CartItemType,
+  {
+    id: 6,
+    title: "Time Machine Sunglass",
+    description: "something",
+    image: "imgs/shades6.jpg",
+    price: 855.00,
+  } as CartItemType,
+  {
+    id: 7,
+    title: "Eternal Now Sunglass",
+    description: "something",
+    image: "imgs/shades7.jpg",
+    price: 999.00,
+  } as CartItemType,
+  {
+    id: 8,
+    title: "Flow Sunglass",
+    description: "something",
+    image: "imgs/shades8.jpg",
+    price: 1260.00,
+  } as CartItemType,
+  {
+    id: 9,
+    title: "Candy Sunglass",
+    description: "something",
+    image: "imgs/shades9.jpg",
+    price: 1225.00,
+  } as CartItemType,
+];
 
 const App = () => {
   const [open, setOpen] = useState(false);
   const [cartItems, setCartItems] = useState<CartItemType[]>([]);
 
-  const { data, isLoading, error } = useQuery<CartItemType[]>(
-    "products",
-    getProducts
-  );
+  // const { data, isLoading, error } = useQuery<CartItemType[]>(
+  //   "products",
+  //   getProducts
+  // );
 
   const getCart = () => {
     const response = fetch(
@@ -39,7 +105,7 @@ const App = () => {
           "Content-Type": "application/json",
         },
       }
-    ).then(res => console.log(res.json())
+    ).then(res => console.log(res)
     );
   };
   const handleAddToCart = (clickItem: CartItemType): void => {
@@ -58,7 +124,7 @@ const App = () => {
         body: JSON.stringify(payload),
       }
     ).then((res) => {
-      console.log(res, payload);
+      console.log(payload);
     });
     setCartItems((prev) => {
       const isItemInCart = prev.find((item) => item.id === clickItem.id);
@@ -94,8 +160,8 @@ const App = () => {
     });
   };
 
-  if (isLoading) return <LinearProgress />;
-  if (error) return <div>somthing went wrong...</div>;
+  // if (isLoading) return <LinearProgress />;
+  // if (error) return <div>somthing went wrong...</div>;
 
   return (
     <Wrapper>
@@ -112,7 +178,7 @@ const App = () => {
         </Badge>
       </StyledButton>
       <Grid container spacing={3}>
-        {data?.map((item) => (
+        {inventory?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
             <Item item={item} handleAddToCart={handleAddToCart} />
           </Grid>
